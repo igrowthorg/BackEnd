d062a5df96e3ab92c9d86d662dfeebbf47f6c1a5
Sathya Hemakanthi <92479405+SathyaHemakanthi@users.noreply.github.com>
2024-01-12 07:43:37 +0530

    change email

--- resource/email.js
+++ resource/email.js
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer';
 export const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "uc.chamod.public@gmail.com",
+      user: "kalanisathya12@gmail.com",
       pass: "jhqwpvtnluihkawp"
     },
   });
